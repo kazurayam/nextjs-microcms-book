67bf7d4e81e2dc4e530e2c6811acb4a7d326357e
kazurayam <kazuaki.urayama@gmail.com>
2024-12-28 22:00:50 +0900

    #4 ContactFormを改造してformrunが生成したフォームをiframe形式で埋め込む形にした

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 - サンプルコード https://github.com/nextjs-microcms-book/nextjs-website-sample
 - 出版社サイト https://www.shoeisha.co.jp/book/detail/9784798183664
+
+- [formrun連携](https://kazurayam.github.io/nextjs-microcms-book/formrun.md)
+
--- my-next-project/app/_components/ContactForm/index.tsx
+++ my-next-project/app/_components/ContactForm/index.tsx
@@ -1,92 +1,47 @@
 "use client";
 
-import { sendGAEvent } from "@next/third-parties/google";
-import { createContactData } from "@/app/_actions/contact";
-import { useFormState } from "react-dom";
 import styles from "./index.module.css";
-
-/**
- * section 9-1-4
- */
-const initialState = {
-  status: "",
-  message: "",
-};
+import { useEffect } from 'react';
 
 export default function ContactForm() {
-  const [state, formAction] = useFormState(createContactData, initialState);
-  console.log(state);
-
-  const handleSubmit = () => {
-    sendGAEvent({ event: "contact", value: "submit" });
+  if (!process.env.NEXT_PUBLIC_FORMRUN_FORM_URL_PATH) {
+    throw new Error("NEXT_PUBLIC_FORMRUN_FORM_URL_PATH is required");
   }
+  useEffect(() => {
+    console.log(`The ContactForm compnent was rendered`)
+    /* generate the following stuff in the DOM
+    <div id="embededForm">
+      <script src="https://sdk.form.run/js/v2/embed.js"></script>
+      <div
+        class="formrun-embed"
+        data-formrun-form=`${NEXT_PUBLIC_FORMRUN_FORM_URL_PATH}`
+        data-formrun-redirect="false">
+      </div>
+    </div>
+    */
+    const form = document.getElementById("embededForm");
+    const containerDiv = form?.querySelector('div'); // child div element
+    if (containerDiv === null) { // insert iframe only if it is not yet there
+      const script = document.createElement("script");
+      script.setAttribute("src", "https://sdk.form.run/js/v2/embed.js");
+      script.async = true;
+      form?.appendChild(script);
 
-  if (state.status === "success") {
-    return (
-      <p className={styles.success}>
-        お問い合わせいただき、ありがとうございます。
-        <br />
-        お返事まで今しばらくお待ちください。
-      </p>
-    );
-  }
+      const embed = document.createElement("div");
+      embed.className = "formrun-embed";
+      embed.setAttribute("data-formrun-form", `${process.env.NEXT_PUBLIC_FORMRUN_FORM_URL_PATH}`);
+      embed.setAttribute("data-formrun-redirect", "false");
+      form?.appendChild(embed);
+
+      return () => {
+        form?.removeChild(script);
+      }
+    }
+  }, []);
+
   return (
-    <form className={styles.form}
-      action={formAction}
-      onSubmit={handleSubmit}
-    >
-      <div className={styles.horizontal}>
-        <div className={styles.item}>
-          <label className={styles.label} htmlFor="lastname">
-            姓
-          </label>
-          <input className={styles.textfield} type="text" id="lastname" name="lastname" />
-        </div>
-      </div>
-
-      <div className={styles.horizontal}>
-        <div className={styles.item}>
-          <label className={styles.label} htmlFor="firstname">
-            名
-          </label>
-          <input className={styles.textfield} type="text" id="firstname" name="firstname" />
-        </div>
-      </div>
-
-      <div className={styles.horizontal}>
-        <div className={styles.item}>
-          <label className={styles.label} htmlFor="company">
-            会社名
-          </label>
-          <input className={styles.textfield} type="text" id="company" name="company" />
-        </div>
-      </div>
-
-      <div className={styles.horizontal}>
-        <div className={styles.item}>
-          <label className={styles.label} htmlFor="email">
-            メールアドレス
-          </label>
-          <input className={styles.textfield} type="text" id="email" name="email" />
-        </div>
-      </div>
-
-      <div className={styles.horizontal}>
-        <div className={styles.item}>
-          <label className={styles.label} htmlFor="message">
-            メッセージ
-          </label>
-          <textarea className={styles.textfield} id="message" name="message" />
-        </div>
-      </div>
-
-      <div className={styles.actions}>
-        {state.status === "error" && (
-          <p className={styles.error}>{state.message}</p>
-        )}
-        <input type="submit" value="送信する" className={styles.button} />
-      </div>
-
-    </form>
+    <>
+      <div id="embededForm" className={styles.form}></div>
+    </>
   );
 }
